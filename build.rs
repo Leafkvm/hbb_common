3161df8d7f2016e61b8396d8dfd6a1a6e2ba5787
user <user@user.com>
2026-03-07 17:06:51 +0800

    Add leafkvm feature

--- build.rs
+++ build.rs
@@ -6,7 +6,12 @@ fn main() {
     protobuf_codegen::Codegen::new()
         .pure()
         .out_dir(out_dir)
-        .inputs(["protos/rendezvous.proto", "protos/message.proto"])
+        .inputs([
+            "protos/rendezvous.proto",
+            "protos/message.proto",
+            #[cfg(feature = "leafkvm")]
+            "protos/leafkvm.proto",
+        ])
         .include("protos")
         .customize(protobuf_codegen::Customize::default().tokio_bytes(true))
         .run()
